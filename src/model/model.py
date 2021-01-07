5d60a5cc975e34bd5e266e675209775c92bbd98c
Scoowy <gahonajuanjo@gmail.com>
2021-01-07 18:29:15 -0500

    Create a MVC template Close #1

---
+++ src/model/model.py
@@ -0,0 +1,62 @@
+from PyQt5.QtCore import QObject, pyqtSignal
+
+
+class Model(QObject):
+    """
+    Model
+    """
+    amount_changed = pyqtSignal(int)
+    even_odd_changed = pyqtSignal(str)
+    enable_reset_changed = pyqtSignal(bool)
+    users_changed = pyqtSignal(list)
+
+    @property
+    def users(self):
+        return self._users
+
+    def add_user(self, value):
+        if value != "":
+            self._users.append(value)
+            self.users_changed.emit(self._users)
+
+    def delete_user(self, value):
+        if value in self._users:
+            del self._users[value]
+
+        self.users_changed.emit(self._users)
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = value
+        self.amount_changed.emit(value)
+
+    @property
+    def even_odd(self):
+        return self._even_odd
+
+    @even_odd.setter
+    def even_odd(self, value):
+        self._even_odd = value
+        self.even_odd_changed.emit(value)
+
+    @property
+    def enable_reset(self):
+        return self._enable_reset
+
+    @enable_reset.setter
+    def enable_reset(self, value):
+        self._enable_reset = value
+        self.enable_reset_changed.emit(value)
+
+    def __init__(self):
+        super().__init__()
+
+        self._amount = 0
+        self._even_odd = ''
+        self._enable_reset = False
+
+        self._users = ["hans"]
---
+++ src/views/main_view.py
@@ -0,0 +1,63 @@
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import pyqtSlot
+from .main_view_ui import Ui_MainWindow
+
+
+class MainView(QMainWindow):
+    """
+    Main view
+    """
+
+    def __init__(self, model, main_controller):
+        """
+        Contructor
+        """
+        super().__init__()
+
+        self._model = model
+        self._main_controller = main_controller
+        self._ui = Ui_MainWindow()
+        self._ui.setupUi(self)
+
+        # connect ui-widget to controller
+        # if ui changes, it sends a signal to an slot on which we connect a controller class.
+        # therefore we can recive the signal in the controller
+        self._ui.spinBox_amount.valueChanged.connect(
+            self._main_controller.change_amount)
+        # Lambda to execute function with value
+        self._ui.pushButton_reset.clicked.connect(
+            lambda: self._main_controller.change_amount(0))
+
+        self._ui.pushButton_add.clicked.connect(
+            lambda: self._main_controller.add_user(self._ui.lineEdit_name.text()))
+        self._ui.pushButton_delete.clicked.connect(
+            lambda: self._main_controller.delete_user(self._ui.listWidget_names.currentRow()))
+
+        # listen for model event signals
+        # connect the method to update the ui to the slots of the model
+        # if model sends/emits a signal the ui gets notified
+        self._model.amount_changed.connect(self.on_amount_changed)
+        self._model.even_odd_changed.connect(self.on_even_odd_changed)
+        self._model.enable_reset_changed.connect(self.on_enable_reset_changed)
+
+        self._model.users_changed.connect(self.on_list_changed)
+
+        # set a default value
+        self._main_controller.change_amount(42)
+
+    @pyqtSlot(int)
+    def on_amount_changed(self, value):
+        self._ui.spinBox_amount.setValue(value)
+
+    @pyqtSlot(str)
+    def on_even_odd_changed(self, value):
+        self._ui.label_even_odd.setText(value)
+
+    @pyqtSlot(bool)
+    def on_enable_reset_changed(self, value):
+        self._ui.pushButton_reset.setEnabled(value)
+
+    @pyqtSlot(list)
+    def on_list_changed(self, value):
+        self._ui.listWidget_names.clear()
+        self._ui.listWidget_names.addItems(value)
