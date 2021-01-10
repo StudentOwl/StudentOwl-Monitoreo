b5ad6d06d608c662e9d8d89c6568fffb250c33e0
Scoowy <gahonajuanjo@gmail.com>
2021-01-10 12:46:06 -0500

    Comment Reader tests

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -2,7 +2,7 @@ import unittest
 from os import path
 
 from src.utils.config import Configuration
-from src.utils.reader import ReaderLogFile
+# from src.utils.reader import ReaderLogFile
 
 
 class TestConfigMethods(unittest.TestCase):
@@ -15,17 +15,17 @@ class TestConfigMethods(unittest.TestCase):
         self.assertIs(type(self.configurator.getSections()), list)
 
 
-class TestReaderMethods(unittest.TestCase):
-    def setUp(self) -> None:
-        self.filePath = path.abspath("./data/9 January,Saturday.htm")
-        self.lastLine = 19
-        self.initLine = 17
-        return super().setUp()
+# class TestReaderMethods(unittest.TestCase):
+#     def setUp(self) -> None:
+#         self.filePath = path.abspath("./data/9 January,Saturday.htm")
+#         self.lastLine = 19
+#         self.initLine = 17
+#         return super().setUp()
 
-    def test_convertLogToJSON(self):
-        reader = ReaderLogFile(self.filePath)
-        jsonText = reader.getJson(reader.getLines())
+#     def test_convertLogToJSON(self):
+#         reader = ReaderLogFile(self.filePath)
+#         jsonText = reader.getJson(reader.getLines())
 
-        self.assertIs(type(jsonText), str)
-        self.assertEqual(reader.lastLine, self.lastLine,
-                         "Numero final de linea incorrecto")
+#         self.assertIs(type(jsonText), str)
+#         self.assertEqual(reader.lastLine, self.lastLine,
+#                          "Numero final de linea incorrecto")
