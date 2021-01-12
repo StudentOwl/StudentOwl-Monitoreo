04a37507edc15c9dacb8fb4a0fc78003a7b24f62
joSar2000 <jorgitosar@gmail.com>
2021-01-12 15:37:52 -0500

    ADD PATH LAUNCH AND FIX METHOD LAUNCH
    Añadimos el PATH antes establecido, además realizamos algunos ajustes dentro del método para el lanzamiento de KidLogger

--- src/utils/launcher.py
+++ src/utils/launcher.py
@@ -1,13 +1,24 @@
 import os
+import subprocess
+from globals import PATH_LAUNCH
 
 class LauncherKidLogger(object):
     """
     Launcher KidLogger Class
     """
 
+    def __init__(self, *args, **kwargs) -> None:  
+        self._args = args
+        self._kwargs = kwargs
+        self._path_launch = PATH_LAUNCH
+
     def launch (self):
         """
         Metodo con el que inicia el arranque de KidLogger
         """
-        path = ""
-        os.system(path + 'kidlogger_user.exe')
+        subprocess.Popen([PATH_LAUNCH + 'kidlogger_user.exe'])
+
+    
+    
+
+
