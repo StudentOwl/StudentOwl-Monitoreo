6deb8c017becccc97fa74835d5817072681f006b
lfbermeo <ferbermeo04@gmail.com>
2021-01-14 00:58:32 -0500

    Create a basic uploader

---
+++ src/utils/uploader.py
@@ -0,0 +1,28 @@
+from configparser import Error
+from urllib3.exceptions import MaxRetryError, NewConnectionError
+from requests.exceptions import ConnectionError
+import json
+import requests
+from requests import Request, Response
+
+def readFile(pathFile: str) -> str:
+    file = ""
+    with open(pathFile, "r", encoding="utf8") as f:
+        lines = f.readlines()
+        for line in lines:
+            file += line
+    return file
+
+def main():
+    file = readFile("./resources/pruebajson.json")
+    # jsonStr = json.loads(file)
+    # print(jsonStr)
+    try:
+        res = requests.get("http://localhost:3000/")
+        print(res.text)
+    except ConnectionError or MaxRetryError or NewConnectionError as err:
+        print("Servidor no disponible")
+
+
+if __name__ == "__main__":
+    main()
