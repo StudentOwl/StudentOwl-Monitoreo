49067006e6a37a10317ad725599f6f252bdef642
lfbermeo <ferbermeo04@gmail.com>
2021-01-14 17:31:09 -0500

    More uploader script

--- src/utils/uploader.py
+++ src/utils/uploader.py
@@ -1,11 +1,13 @@
 from configparser import Error
+from requests.api import options
 from urllib3.exceptions import MaxRetryError, NewConnectionError, RequestError
 from requests.exceptions import ConnectionError
 import json
 import requests
 from requests import Request, Response
 
-def readFile(pathFile: str) -> str:
+
+def readJsonFile(pathFile: str) -> str:
     file = ""
     with open(pathFile, "r", encoding="utf8") as f:
         lines = f.readlines()
@@ -13,21 +15,65 @@ def readFile(pathFile: str) -> str:
             file += line
     return file
 
+
 def verifyServer(server: str) -> bool:
+    """
+    Metodo que verifica que el servidor se encuentre online
+    """
+    res = None
     try:
-        pass
+        res = requests.get(server)
     except RequestError as err:
-        pass
+        print("[ERROR]: Server don't response")
+
+    if res:
+        return res.status_code == 200
+
+    return False
+
+
+def uploadLog(url: str, payload) -> bool:
+    """
+    Metodo que realiza la subida de informacion a la API
+    """
+
+    options = {"url": url}
+
+    if type(payload) is dict:
+        options["data"] = payload
+    elif type(payload) is str:
+        options["json"] = payload
+    else:
+        return False
+
+    res = None
+    try:
+        res = requests.post(**options)
+    except RequestError as err:
+        print("[ERROR]: Error uploading information.")
+
+    if res:
+        resjson = res.json()
+        print(resjson["message"])
+        return True
+    else:
+        print(res.status_code)
+
+    return False
+
 
 def main():
-    file = readFile("./resources/pruebajson.json")
-    # jsonStr = json.loads(file)
-    # print(jsonStr)
-    try:
-        res = requests.get("http://localhost:3000/")
-        print(res.text)
-    except ConnectionError or MaxRetryError or NewConnectionError as err:
-        print("Servidor no disponible")
+    file = readJsonFile("./resources/pruebajson.json")
+    server = "http://localhost:3000/"
+    route = "api/v1.0/GSPR01/lfbermeo"
+    
+    if verifyServer(server):
+        res = uploadLog(f"{server}{route}", file)
+
+        if res:
+            print("[INFO]: Exitoso")
+        else:
+            print("[ERROR]: Error")
 
 
 if __name__ == "__main__":
