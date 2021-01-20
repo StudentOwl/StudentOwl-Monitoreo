923369e2d6b4c224276f7be68c8478d62fee8d14
Scoowy <gahonajuanjo@gmail.com>
2021-01-20 14:36:36 -0500

    Create bat files

---
+++ scripts/step2.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+:: This batch file init the Step 2 for StudentOwl - Monitoreo
+TITLE StudentOwl-Monitoreo Step 2
+ECHO Please wait...
+ECHO Start Step 2
+START ../programs/Step-2.exe
+TIMEOUT 1
+START ../programs/Step-3.exe
+ECHO Complete...
+ECHO Exit...
